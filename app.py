c08d0a98419c7840bb1e3566a2965e5883412248
? <anirudhp@technocrafts.co.in>
2025-04-15 12:21:16 +0530

    changes

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow import keras
 from tensorflow.keras import layers  # type: ignore
+import traceback
 
 
 app = Flask(__name__)
