41bdcd530e746ab77aed4ed9a53538448359899f
? <anirudhp@technocrafts.co.in>
2025-05-10 17:37:26 +0530

    integrated getCoalBlendedProperties

--- app.py
+++ app.py
@@ -200,6 +200,19 @@ def get_coke_properties_csv():
     return csv_output
 
 
+def get_blended_coal_properties_csv():
+    response = post_encrypted('http://3.111.89.109:3000/api/getBlendedCoalPropertiescsv',{"companyId":1})
+
+    x = response
+    y = x[0][0]['csv_output']
+    csv_rows = y.split('\n')
+    csv_output = '\n'.join(csv_rows)
+    print(csv_output)
+
+    return csv_output
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -969,8 +982,9 @@ for i in range(D_tensor.shape[0]):
             daily_vectors.append(tf.stack(row_vector))
 daily_vectors_tensor = tf.stack(daily_vectors)        
 input_data = tf.reshape(daily_vectors_tensor, [-1, 14])
-daily_vectors_flattened = daily_vectors_tensor.numpy().reshape(52, -1) 
-Blended_coal_parameters = np.loadtxt('blended_coal_data.csv', delimiter=',')
+daily_vectors_flattened = daily_vectors_tensor.numpy().reshape(52, -1)
+anir = get_blended_coal_properties_csv()
+Blended_coal_parameters = np.loadtxt(io.StringIO(anir), delimiter=',')
 input_train, input_test, target_train, target_test = train_test_split(
             daily_vectors_tensor.numpy(), Blended_coal_parameters, test_size=0.2, random_state=42
         )       
