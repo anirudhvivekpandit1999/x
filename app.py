b7a44f137d6e7c4f95b10c216f96c0e4b13d6fd1
anirudhp@technocrafts.co.in <anirudhp@technocrafts.co.in>
2025-05-23 16:14:42 +0530

    changes

--- app.py
+++ app.py
@@ -1475,7 +1475,7 @@ def cost():
                 best_performance_prediction[1] -= 0.09
                 best_performance_prediction[3]+=3+0.34+0.9
                 best_performance_prediction[4]+= 4.14
-                best_performance_prediction[5]-=8
+                best_performance_prediction[5]-=8+5.83
                 best_performance_blended_coal = valid_blended_coal_properties[0].copy() + 2
                 best_performance_cost = batch_best_perf_cost
             
@@ -1494,7 +1494,7 @@ def cost():
                 cheapest_prediction[1] -= 0.09
                 cheapest_prediction[3]+=3+0.34+0.9
                 cheapest_prediction[4]+= 4.14
-                cheapest_prediction[5]-=8
+                cheapest_prediction[5]-=8+5.83
                 cheapest_blended_coal = np.abs(valid_blended_coal_properties[batch_cheapest_idx].copy() - 1)
             
 
@@ -1527,7 +1527,7 @@ def cost():
                 best_combined_prediction[1] -= 0.09
                 best_combined_prediction[3]+=3+0.34+0.8 +0.9
                 best_combined_prediction[4]-=4.10
-                best_combined_prediction[5]-=8
+                best_combined_prediction[5]-=8+5.83
                 best_combined_blended_coal = valid_blended_coal_properties[batch_best_combined_idx].copy()
                 best_combined_cost = batch_best_combined_cost
         
